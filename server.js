9f75b78eee71be574b95834b9427d2904ffa5c50
jondcoleman <jcoleman3307@gmail.com>
2016-01-03 12:47:46 -0500

    initial changes

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var express = require('express');
+var mongo = require('mongodb');
+var routes = require('./app/routes/index.js');
+
+var app = express();
+require('dotenv').load();
+
+mongo.connect(process.env.MONGOLAB_URI, function (err, db) {
+
+   if (err) {
+      throw new Error('Database failed to connect!');
+   } else {
+      console.log('Successfully connected to MongoDB on port 27017.');
+   }
+
+   app.use('/public', express.static(process.cwd() + '/public'));
+
+   routes(app, db);
+
+   app.listen(process.env.PORT || 3000, function () {
+      console.log('Node.js listening on port 3000...');
+   });
+
+});
